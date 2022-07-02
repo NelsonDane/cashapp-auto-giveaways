5b832010dbb74ec56238e38573304887d2ce4cea
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-07-01 23:06:18 -0400

    Delete PyTextNow_API directory

--- PyTextNow_API/examples/get_unread_messages.py
+++
@@ -1,9 +0,0 @@
-import pytextnow as pytn
-
-client = pytn.Client("username") # You can also include the cookie in ther Client constructor
-# Here you should input your connect.sid cookie
-
-unreads = client.get_unread_messages()
-
-for msg in unreads:
-    print(msg)
